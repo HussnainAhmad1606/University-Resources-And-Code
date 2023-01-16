d7a0d90d3cc423af6649aeccecb588fc24daaf51
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2023-01-16 14:17:23 +0500

    Update Sum_Of_Prime_Numbers_From_1_to_100.cpp

--- C++ Practice Questions/Sum_Of_Prime_Numbers_From_1_to_100.cpp
+++ C++ Practice Questions/Sum_Of_Prime_Numbers_From_1_to_100.cpp
@@ -1,19 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int a,b,sum;
-	for(a=1;a<=100;a++)
-	{
-		b=a/2;
-		if(a%b==0)
-		{
-			continue;
-		}
-		else {sum=a;
-		}
-		sum =sum+a;
-		
-	}
-cout<<sum;
-cout << "F";
+int main() {
+   int sum = 0;
+   bool isPrime = true;
+   for (int number = 2; number <= 100; number++) {
+       for (int i = 2; i <= (number-1); i++) {
+           if (number % i == 0) {
+               isPrime = false;
+               break;
+           }
+           else {
+               isPrime = true;
+           }
+       }
+       
+       if  (isPrime) {
+           sum += number;
+       }
+   }
+   cout << sum;
 }
