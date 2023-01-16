4cb0856583902557d7a6e15ccb9fc5ebcc26e74d
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2023-01-16 12:42:53 +0500

    Add files via upload

---
+++ C++ Practice Questions/Sum_Of_Prime_Numbers_From_1_to_100.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,sum;
+	for(a=1;a<=100;a++)
+	{
+		b=a/2;
+		if(a%b==0)
+		{
+			continue;
+		}
+		else {sum=a;
+		}
+		sum =sum+a;
+		
+	}
+cout<<sum;
+cout << "F";
+}
