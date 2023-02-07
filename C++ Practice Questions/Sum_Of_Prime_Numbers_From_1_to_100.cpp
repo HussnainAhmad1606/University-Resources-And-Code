55ea4885e6e8ee9b737ad63f268d2295dda6aa5a
Salman <101444405+salmannaeem112215@users.noreply.github.com>
2023-02-07 04:48:22 -0800

    Make Functions
    WBYKT

--- C++ Practice Questions/Sum_Of_Prime_Numbers_From_1_to_100.cpp
+++ C++ Practice Questions/Sum_Of_Prime_Numbers_From_1_to_100.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
+
+int primtSumTill(int n);
+
 int main() {
-   int sum = 0;
+   cout << printSumTill(100);
+}
+
+int primtSumTill(int n){
+   if(n<2) return 0;
+   int sum=0;
    bool isPrime = true;
-   for (int number = 2; number <= 100; number++) {
+   for (int number = 2; number <= n; number++) {
        for (int i = 2; i <= (number-1); i++) {
            if (number % i == 0) {
                isPrime = false;
@@ -18,5 +26,6 @@ int main() {
            sum += number;
        }
    }
-   cout << sum;
+   
+ return sum;  
 }
